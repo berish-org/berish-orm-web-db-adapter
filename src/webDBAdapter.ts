fe442ce540bcee893c6ebf56945160d71eeadb81
Berishev Ravil <quadraticteam@gmail.com>
2021-03-09 17:12:52 +0300

    Reconnect subscribe + smart subscribers

--- src/webDBAdapter.ts
+++ src/webDBAdapter.ts
@@ -1,69 +1,139 @@
-import Emitter from '@berish/emitter';
+import { EventEmitter } from '@berish/emitter';
+import LINQ from '@berish/linq';
 import { BaseDBAdapter, IBaseDBItem, Query, QueryData, QueryDataSchema } from '@berish/orm';
-import tryCall from '@berish/try-call';
+
+import type { WebDBMethodParameters, WebDBMethodReturn } from './webDBReceiver';
 
 export interface IWebDBAdapterParams {
-  sendData: <T>(path: string, args: any[]) => Promise<T>;
+  get: (...args: WebDBMethodParameters<'get'>) => WebDBMethodReturn<'get'>;
+  create: (...args: WebDBMethodParameters<'create'>) => WebDBMethodReturn<'create'>;
+  update: (...args: WebDBMethodParameters<'update'>) => WebDBMethodReturn<'update'>;
+  delete: (...args: WebDBMethodParameters<'delete'>) => WebDBMethodReturn<'delete'>;
+  count: (...args: WebDBMethodParameters<'count'>) => WebDBMethodReturn<'count'>;
+  find: (...args: WebDBMethodParameters<'find'>) => WebDBMethodReturn<'find'>;
+  index: (...args: WebDBMethodParameters<'index'>) => WebDBMethodReturn<'index'>;
+  subscribe: (...args: WebDBMethodParameters<'subscribe'>) => WebDBMethodReturn<'subscribe'>;
+}
+
+export interface IWebDBAdapterConstructorParams {
+  isConnected: () => boolean;
 }
 
 export class WebDBAdapter extends BaseDBAdapter<IWebDBAdapterParams> {
-  private emitter: Emitter<any> = null;
+  private _emitter: EventEmitter<any> = null;
+  private _constructorParams: IWebDBAdapterConstructorParams = null;
 
-  public async initialize(params: IWebDBAdapterParams) {
+  // Здесь хранятся кникальные запросы к списку callback (по queryHash)
+  private _uniqueQueries: QueryData<QueryDataSchema>[] = [];
+
+  constructor(constructorParams: IWebDBAdapterConstructorParams) {
+    super();
+
+    if (!constructorParams) throw new TypeError('webDBAdapter constructorParams is null');
+    this._constructorParams = constructorParams;
+    this._uniqueQueries = [];
+  }
+
+  public initialize = async (params: IWebDBAdapterParams) => {
     this.params = params;
-    this.deepFetch = true;
-    this.emitter = new Emitter();
-  }
+    this._emitter = new EventEmitter();
+  };
 
-  public emptyFieldLiteral() {
-    return void 0;
-  }
+  public close = async () => {
+    this.params = null;
+    this._emitter.offAll();
+  };
 
   public count(query: QueryData<QueryDataSchema>): Promise<number> {
-    return this.params.sendData<number>('count', [query]);
+    if (!this.params.count) throw new TypeError('webDBAdapter params.count is null');
+    return this.params.count(query);
   }
 
-  public async get<T>(query: QueryData<QueryDataSchema>) {
-    return this.params.sendData<T>('get', [query]);
-  }
+  public get = (query: QueryData<QueryDataSchema>) => {
+    if (!this.params.get) throw new TypeError('webDBAdapter params.get is null');
 
-  public async create(tableName: string, items: IBaseDBItem[]) {
-    return this.params.sendData<void>('create', [tableName, items]);
-  }
+    return this.params.get(query);
+  };
 
-  public async update(tableName: string, items: IBaseDBItem[]) {
-    return this.params.sendData<void>('update', [tableName, items]);
-  }
+  public create = (tableName: string, items: IBaseDBItem[]) => {
+    if (!this.params.create) throw new TypeError('webDBAdapter params.create is null');
 
-  public async delete(data: QueryData<QueryDataSchema>) {
-    return this.params.sendData<void>('delete', [data]);
-  }
+    return this.params.create(tableName, items);
+  };
 
-  public async index(tableName: string, indexName: string, keys?: string[]) {
-    return this.params.sendData<void>('index', [tableName, indexName, keys]);
-  }
+  public update = (tableName: string, items: IBaseDBItem[]) => {
+    if (!this.params.update) throw new TypeError('webDBAdapter params.update is null');
 
-  public async find<T>(query: QueryData<QueryDataSchema>) {
-    return this.params.sendData<T>('find', [query]);
-  }
+    return this.params.update(tableName, items);
+  };
 
-  public subscribe<T>(query: QueryData<QueryDataSchema>, cb: (oldVal: T, newVal: T) => any) {
-    const hash = Query.getHash(query);
-    const methodName = 'subscribe';
-    const eventName = `${hash}_${methodName}`;
-    const eventHash = this.emitter.cacheSubscribe<{ oldValue: T; newValue: T }>(
-      eventName,
-      async callback => {
-        const unlistener = await this.params.sendData<() => Promise<void>>('subscribe', [
-          query,
-          (oldValue: T, newValue: T) => callback({ oldValue, newValue }),
-        ]);
-        return async () => {
-          await tryCall(() => unlistener(), { maxAttempts: 5, timeout: 1000 });
-        };
-      },
-      ({ oldValue, newValue }) => cb(oldValue, newValue),
+  public delete = (query: QueryData<QueryDataSchema>) => {
+    if (!this.params.delete) throw new TypeError('webDBAdapter params.delete is null');
+
+    return this.params.delete(query);
+  };
+
+  public index = (tableName: string, indexName: string, keys?: string[]) => {
+    if (!this.params.index) throw new TypeError('webDBAdapter params.index is null');
+
+    return this.params.index(tableName, indexName, keys);
+  };
+
+  public find = (query: QueryData<QueryDataSchema>) => {
+    if (!this.params.find) throw new TypeError('webDBAdapter params.find is null');
+
+    return this.params.find(query);
+  };
+
+  public subscribe = (
+    data: QueryData<QueryDataSchema>,
+    callback: (oldValue: IBaseDBItem, newValue: IBaseDBItem) => any,
+  ) => {
+    if (!this.params.subscribe) throw new TypeError('webDBAdapter params.subscribe is null');
+
+    const queryHash: string = Query.getHash(data);
+
+    if (!this._emitter.hasEvent(queryHash)) {
+      this._realSubscribe(data);
+    }
+
+    const eventHash = this._emitter.on(queryHash, ({ oldValue, newValue }) => callback(oldValue, newValue));
+    return () => {
+      this._emitter.off(eventHash);
+    };
+  };
+
+  public connect = () => {
+    // Здесь мы подключаем все реальные callback к текущим
+    const eventNames = LINQ.from(this._emitter['_events'])
+      .map((m) => m.eventName)
+      .distinct((m) => m);
+
+    for (const queryHash of eventNames) {
+      if (!this._emitter.hasEvent(queryHash)) {
+        const data = this._uniqueQueries.filter((m) => Query.getHash(m) === queryHash)[0];
+        if (data) this._realSubscribe(data);
+      }
+    }
+  };
+
+  private _realSubscribe = (data: QueryData<QueryDataSchema>) => {
+    const queryHash: string = Query.getHash(data);
+
+    // Добавляем Query к списку уникальных
+    if (this._uniqueQueries.filter((m) => Query.getHash(m) === queryHash).length <= 0) this._uniqueQueries.push(data);
+
+    const unsubcribe = this.params.subscribe(data, (oldValue, newValue) =>
+      this._emitter.emitSync(queryHash, { oldValue, newValue }),
     );
-    return () => this.emitter.off(eventHash);
-  }
+
+    const triggerOffEventHash = this._emitter.triggerOffEvent(queryHash, () => {
+      // Удаляем Query из списка уникальных
+      this._uniqueQueries.filter((m) => Query.getHash(m) !== queryHash);
+
+      if (this._constructorParams.isConnected()) unsubcribe();
+
+      this._emitter.offTriggerOff(triggerOffEventHash);
+    });
+  };
 }
--- src/webDBReceiver.ts
+++ src/webDBReceiver.ts
@@ -1,72 +1,120 @@
-import { baseDBMethods, Entity, Manager, Query, QueryData, QueryDataSchema } from '@berish/orm';
+import { CacheEmitter } from '@berish/emitter';
+import { IBaseDBItem, Manager, Query } from '@berish/orm';
+import { IWebDBAdapterParams } from './webDBAdapter';
 
-interface IDbReceiverParams {
-  by?: <T extends Entity>(query: Query<T>) => Query<T> | Promise<Query<T>>;
+export type WebDBMethodParameters<N extends keyof Manager['db']> = Parameters<Manager['db'][N]>;
+export type WebDBMethodReturn<N extends keyof Manager['db']> = ReturnType<Manager['db'][N]>;
+
+interface SubscriptionEventMap {
+  [queryHash: string]: { oldValue: any; newValue: any };
 }
-type dbMethodType<N extends keyof Manager['db']> = Parameters<Manager['db'][N]>;
 
 export class WebDBReceiver {
   private _manager: Manager = null;
-  private _params: IDbReceiverParams = null;
+  // private _subscriptionEmitter = new EventEmitter<SubscriptionEventMap>();
+  private _cacheEmitter = new CacheEmitter();
+  private _subscribeEventHashes: string[] = [];
 
-  constructor(manager: Manager, params?: IDbReceiverParams) {
+  constructor(manager: Manager) {
     this._manager = manager;
-    this._params = params;
   }
 
   get manager() {
     return this._manager;
   }
 
-  get params() {
-    return this._params || {};
+  public get API(): IWebDBAdapterParams {
+    return {
+      get: this.get,
+      create: this.create,
+      update: this.update,
+      delete: this.delete,
+      count: this.count,
+      find: this.find,
+      index: this.index,
+      subscribe: this.subscribe,
+    };
   }
 
-  public async call<T extends keyof Manager['db']>(name: T, ...args: any[]) {
-    if (baseDBMethods.indexOf(name) === -1) throw new Error('orm db method not found');
-    if (name === 'count') {
-      const [query] = args as dbMethodType<'count'>;
-      const queryWithBy = await this.withBy(query);
-      return this.manager.db.count(queryWithBy);
-    }
-    if (name === 'get') {
-      const [query] = args as dbMethodType<'get'>;
-      const queryWithBy = await this.withBy(query);
-      return this.manager.db.get(queryWithBy);
-    }
-    if (name === 'create') {
-      const [table, items] = args as dbMethodType<'create'>;
-      return this.manager.db.create(table, items);
-    }
-    if (name === 'update') {
-      const [table, items] = args as dbMethodType<'update'>;
-      return this.manager.db.update(table, items);
-    }
-    if (name === 'delete') {
-      const [queryData] = args as dbMethodType<'delete'>;
-      const queryWithBy = await this.withBy(queryData);
-      return this.manager.db.delete(queryWithBy);
-    }
-    if (name === 'index') {
-      const [table, indexName, keys] = args as dbMethodType<'index'>;
-      return this.manager.db.index(table, indexName, keys);
-    }
-    if (name === 'find') {
-      const [queryData] = args as dbMethodType<'find'>;
-      const queryWithBy = await this.withBy(queryData);
-      return this.manager.db.find(queryWithBy);
-    }
-    if (name === 'subscribe') {
-      const [queryData, cb] = args as dbMethodType<'subscribe'>;
-      const queryWithBy = await this.withBy(queryData);
-      return this.manager.db.subscribe(queryWithBy, cb);
-    }
-  }
+  public count = (...[query]: WebDBMethodParameters<'count'>) => {
+    return this.manager.db.count(query);
+  };
 
-  private async withBy(queryData: QueryData<QueryDataSchema>) {
-    if (!queryData) throw new Error('FP-ORM: queryData is undefined');
-    const query = Query.fromJSON(queryData);
-    const withByQuery = (await (this.params.by && this.params.by(query))) || query;
-    return withByQuery.json;
-  }
+  public get = (...[query]: WebDBMethodParameters<'get'>) => {
+    return this.manager.db.get(query);
+  };
+
+  public create = (...[table, items]: WebDBMethodParameters<'create'>) => {
+    return this.manager.db.create(table, items);
+  };
+
+  public update = (...[table, items]: WebDBMethodParameters<'update'>) => {
+    return this.manager.db.update(table, items);
+  };
+
+  public delete = (...[queryData]: WebDBMethodParameters<'delete'>) => {
+    return this.manager.db.delete(queryData);
+  };
+
+  public index = (...[table, indexName, keys]: WebDBMethodParameters<'index'>) => {
+    return this.manager.db.index(table, indexName, keys);
+  };
+
+  public find = (...[queryData]: WebDBMethodParameters<'find'>) => {
+    return this.manager.db.find(queryData);
+  };
+
+  // public subscribe = (...[queryData, callback]: WebDBMethodParameters<'subscribe'>) => {
+  //   const queryHash = Query.getHash(queryData);
+
+  //   if (!this._subscriptionEmitter.hasEvent(queryHash)) {
+  //     console.log('REAL SUBSCRIBE');
+  //     const unsubcribe = this.manager.db.subscribe(queryData, (oldValue, newValue) => {
+  //       this._subscriptionEmitter.emitSync(queryHash, { oldValue, newValue });
+  //     });
+
+  //     const triggerOffEventHash = this._subscriptionEmitter.triggerOffEvent(queryHash, () => {
+  //       console.log('REAL UNSUBSCRIBE');
+  //       unsubcribe();
+
+  //       this._subscriptionEmitter.offTriggerOff(triggerOffEventHash);
+  //     });
+  //   }
+
+  //   console.log('EVENT SUBSCRIBE');
+  //   const eventHash = this._subscriptionEmitter.on(queryHash, ({ oldValue, newValue }) => callback(oldValue, newValue));
+
+  //   return () => this._subscriptionEmitter.off(eventHash);
+  // };
+
+  public subscribe = (...[queryData, callback]: WebDBMethodParameters<'subscribe'>) => {
+    const queryHash = Query.getHash(queryData);
+
+    const eventHash = this._cacheEmitter.subscribe<{ oldValue: IBaseDBItem; newValue: IBaseDBItem }>(
+      `subscribe_${queryHash}`,
+      callback => {
+        const unsubscribe = this.manager.db.subscribe(queryData, (oldValue, newValue) =>
+          callback({ oldValue, newValue }),
+        );
+        return () => {
+          unsubscribe();
+        };
+      },
+      ({ oldValue, newValue }) => callback(oldValue, newValue),
+    );
+
+    this._subscribeEventHashes.push(eventHash);
+
+    return () => {
+      this._subscribeEventHashes = this._subscribeEventHashes.filter(m => m !== eventHash);
+      this._cacheEmitter.unsubscribe(eventHash);
+    };
+  };
+
+  public close = () => {
+    // this._subscriptionEmitter.offAll();
+    for (const hash of this._subscribeEventHashes) {
+      this._cacheEmitter.unsubscribe(hash);
+    }
+  };
 }
